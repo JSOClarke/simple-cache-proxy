935c13cfc62ffac20afc52d586c5a8c05b6d7e95
Jordan <jsoclarke@gmail.com>
2025-09-13 19:07:38 +0100

    Completed the proxybackend

--- index.js
+++ index.js
@@ -6,7 +6,57 @@ import fetch from "node-fetch";
 import chalk from "chalk";
 import fs from "fs";
 
+const filePath = "./cache.json";
+
 const program = new Command();
+
+function loadCache() {
+  // try and load the cache file its its not there create one from scratch
+
+  try {
+    if (!fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, "{}", "utf8");
+      //read the file
+    }
+    const raw = fs.readFileSync(filePath, "utf-8");
+
+    const cache = JSON.parse(raw);
+
+    // console.log(cache);
+    // cache is now a js object
+    return cache;
+  } catch (err) {
+    console.error("Failed to load the cache");
+    process.exit(1);
+  }
+}
+
+function saveCache(cache) {
+  try {
+    const stringCache = JSON.stringify(cache, null, 2);
+    fs.writeFileSync(filePath, stringCache, "utf-8");
+    console.log("Succesfully writen to cache");
+  } catch (err) {
+    console.error("Failed saving to cache");
+  }
+}
+function clearCache() {
+  try {
+    if (fs.existsSync(filePath)) {
+      fs.writeFileSync(filePath, "{}", "utf-8");
+      console.log("Succesfully cleared the cache down");
+      process.exit(0);
+    } else {
+      console.error(
+        "No Cache file to clear down please run the service to create one and rerun this command"
+      );
+      process.exit(1);
+    }
+  } catch (err) {
+    console.error(`Failed to clear the cache, please check ${filePath} `);
+    process.exit(1);
+  }
+}
 
 program
   .option(
@@ -18,33 +68,36 @@ program
     "Specifiy the URL of the server to which the requests will be forwarded. "
   );
 
-const option = program.opts();
+program.option("--clear-cache", "Clear the cache for the proxy server");
 
 program.parse(process.argv);
+const option = program.opts();
+
+if (option.clearCache) {
+  clearCache();
+  process.exit(0);
+}
 console.log(`Port chosen: ${option.port}, ${option.origin}`);
-const app = express();
 
 let numbericPort = Number(option.port);
 // validate the input
 
-// if (isNaN(numbericPort)) {
-//   console.error("Port number needed to be a Number");
-//   process.exit(1);
-// }
+if (isNaN(numbericPort) && numbericPort > 3000 && numbericPort < 5000) {
+  console.error("Port number needed to be a Number");
+  process.exit(1);
+}
 
-// if (isNaN(numbericPort)) {
-//   console.error("Port number needed to be a Number");
-//   process.exit(1);
-// }
+const originIncludes = "https://";
+
+if (!option.origin.includes(originIncludes)) {
+  console.error(`origin needs to include ${originIncludes}`);
+  process.exit(1);
+}
+
+const app = express();
 
 // start the express server
-
-const log_data = "This is the log data";
-
-let requestCount = 0;
-
-let cache = {};
-console.log("cache", cache);
+let cache = loadCache();
 
 app.get(/.*/, async (req, res) => {
   const url = req.url;
@@ -63,11 +116,8 @@ app.get(/.*/, async (req, res) => {
     return res.status(200).json(cache[url].body);
   }
 
-  console.log("path", url);
-
   const query = `${option.origin}${url}`;
-  requestCount++;
-  console.log(`Request Number: ${requestCount}, to URL: ${query}`);
+  //   console.log(`Request Number: ${requestCount}, to URL: ${query}`);
   try {
     const result = await fetch(query);
 
@@ -91,6 +141,7 @@ app.get(/.*/, async (req, res) => {
     //Cache the reponse (header and body)
 
     cache[url] = { body: data, header: resultHeader };
+    saveCache(cache);
     res.set("X-Cache", "MISS");
     res.status(200).json(data);
   } catch (err) {
